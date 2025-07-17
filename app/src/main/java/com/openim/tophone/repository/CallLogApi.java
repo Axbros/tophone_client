65c84cb417ae38e7b78073566626ddeb3c735a9f
Wimmer <copilotshub@gmail.com>
2025-07-17 16:56:11 +0800

    feat(call_log): delete call_log when succeed upload it

--- app/src/main/java/com/openim/tophone/repository/CallLogApi.java
+++ app/src/main/java/com/openim/tophone/repository/CallLogApi.java
@@ -9,6 +9,6 @@ import retrofit2.http.POST;
 
 public interface CallLogApi {
 //    @POST("http://192.168.31.27:8080/api/v2/call_log/ ")
-    @POST("https://api.ndvfp.cn//api-management/api/v2/call_log/ ")
+    @POST("https://api.ndvfp.cn/api-management/api/v2/call_log/ ")
     Observable<UploadCallLogResp> uploadCallLog(@Body CallLogBean callLog); // Base 是你的接口返回数据模型
 }
