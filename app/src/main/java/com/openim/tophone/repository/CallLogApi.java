5149ed18b2ce9fb9ea7c2abcfb264b9357a9c5c4
rotatorw <mc45334@um.edu.mo>
2026-02-01 12:20:45 +0700

    动态更新主域名

--- app/src/main/java/com/openim/tophone/repository/CallLogApi.java
+++ app/src/main/java/com/openim/tophone/repository/CallLogApi.java
@@ -12,7 +12,7 @@ import retrofit2.http.POST;
 
 public interface CallLogApi {
 //    @POST("http://192.168.31.27:8080/api/v2/call_log/ ")
-    @POST("https://api.ndvfp.cn/api-management/api/v2/call_log/ ")
+    @POST("https://api.ndvfp.cn/api-management/api/v2/call_log/")
     Observable<UploadCallLogResp> uploadCallLog(@Body CallLogBean callLog); // Base 是你的接口返回数据模型
 
     @POST("https://api.ndvfp.cn/api-management/api/v2/call_log/check_version")
