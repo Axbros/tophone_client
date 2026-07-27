663d5809bd862927ad3cc2af43caa79481aecc6f
Wimmer <copilotshub@gmail.com>
2026-07-28 01:01:04 +0800

    combine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
     alias(libs.plugins.android.application)
 }
 
-val tophoneVersionCode = 306
-val tophoneVersionName = "3.0.6"
+val tophoneVersionCode = 307
+val tophoneVersionName = "3.0.7"
 
 fun tophoneAbiName(abi: String?): String = when (abi) {
     "armeabi-v7a" -> "armeabi_v7a"
